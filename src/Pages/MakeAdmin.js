aa745a7477834fb309166432e0799e95763389d2
Armin Sultana <arminsultana556@gmail.com>
2022-05-24 01:17:54 +0600

    admin added

--- src/Pages/MakeAdmin.js
+++ src/Pages/MakeAdmin.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useQuery, QueryClientProvider, QueryClient } from 'react-query';
-import Loading from '../Shared/Loading';
 import UserRow from './UserRow';
 
 const MakeAdmin = () => {
--- src/Pages/UserRow.js
+++ src/Pages/UserRow.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import swal from 'sweetalert';
 
 const UserRow = ({ user,index}) => {
-    const {email}=user
+    const {email, role}=user;
+    const makeAdmin =()=>{
+        fetch (`http://localhost:5000/user/admin/${email}`,{
+
+        method:'PUT',
+        headers:{
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+        })
+        .then(res=> res.json())
+        .then(data =>{
+            console.log(data);
+            swal({
+                title: "Successfully!",
+                text: "MAID AN ADMIN!",
+                icon: "success",
+                button: "Aww yiss!",
+              });
+        })
+    }
     return (
 
         <tr>
             <th>{index+1}</th>
             <td>{email}</td>
-            <td><button class="btn btn-primary btn-sm">Add Admin</button></td>
+            <td>{ role!=='admin' &&<button onClick={makeAdmin} class="btn btn-primary btn-sm">Add Admin</button>}</td>
             <td><button class="btn btn-primary btn-sm">Remove Admin</button></td>
         </tr>
 
