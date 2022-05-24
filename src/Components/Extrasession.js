d99abd0234471f07d55f3c5488761e2441c13682
Armin Sultana <arminsultana556@gmail.com>
2022-05-24 18:37:30 +0600

    extra session added

---
+++ src/Components/Extrasession.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Extrasession = () => {
+    return (
+        <div>
+            <h1>Your payment mathod</h1>
+            
+        </div>
+    );
+};
+
+export default Extrasession;
